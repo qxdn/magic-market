be4aecc1993d4263e5c08aa74b019b76e9a2b6a5
Xu Qian <1464238196@qq.com>
2024-05-31 16:41:02 +0800

    准备重构

--- main.py
+++ main.py
@@ -2,11 +2,40 @@ import requests
 from typing import List, Dict
 import time
 import click
+import logging
+import sys
+
+# 配置logging模块
+logging.basicConfig(filename="dev.txt", level=logging.DEBUG)
+fmt = logging.Formatter(
+    "%(name)s: %(asctime)s | %(levelname)s | %(filename)s:%(lineno)s | %(process)d >>> %(message)s"
+)
+# handler
+result_handler = logging.FileHandler("result.txt")
+result_handler.setLevel(logging.INFO)
+log_handler = logging.FileHandler("log.txt")
+log_handler.setLevel(logging.INFO)
+debug_handler = logging.FileHandler("debug.txt")
+debug_handler.setLevel(logging.DEBUG)
+debug_handler.setFormatter(fmt)
+std_handler = logging.StreamHandler(sys.stdout)
+std_handler.setLevel(logging.INFO)
+
+# logger
+common_logger = logging.getLogger("common")
+common_logger.addHandler(log_handler)
+common_logger.addHandler(debug_handler)
+common_logger.addHandler(std_handler)
+result_logger = logging.getLogger("result")
+result_logger.addHandler(result_handler)
+
+
+
 
 MARKET_URL = "https://mall.bilibili.com/mall-magic-c/internet/c2c/v2/list"
 
 HEADERS = {
-    "Cookie": "", #  TODO: 填写cookie
+    "Cookie": "",  #  TODO: 填写cookie
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
 }
 
@@ -58,29 +87,47 @@ def get_market_data(
         },
     )
     # print(response.content)
+    common_logger.debug(f"response content:{response.content}")
     return response.json()
 
 
 def filter_data(data: dict, keyword: str = None):
+    result = []
     if keyword is None:
-        return
+        return result
     for item in data:
         c2cItemsId = item.get("c2cItemsId", None)
         showPrice = item.get("showPrice", None)
         c2cItemsName = item.get("c2cItemsName", None)
         if keyword in c2cItemsName:
-            print(f"{c2cItemsName} - {showPrice}元 - {process_url(c2cItemsId)}")
+            common_logger.info(
+                f"find {keyword}:{c2cItemsName} - {showPrice}元 - {process_url(c2cItemsId)}"
+            )
+            result.append(
+                {
+                    f"{c2cItemsName} - {showPrice}元 - {process_url(c2cItemsId)}": showPrice
+                }
+            )
             continue
         for detail in item.get("detailDtoList", []):
             name = detail.get("name", "")
             if keyword in name:
-                print(f"{c2cItemsName} - {showPrice}元 - {process_url(c2cItemsId)}")
+                common_logger.info(
+                    f"find {keyword}:{c2cItemsName} - {showPrice}元 - {process_url(c2cItemsId)}"
+                )
+                result.append(
+                    {
+                        f"{c2cItemsName} - {showPrice}元 - {process_url(c2cItemsId)}": showPrice
+                    }
+                )
                 break
+    return result
 
 
 def search(keyword: str, category: str, sort: str):
     nextId = None
     count = 0
+    result = []
     while True:
         data = get_market_data(
             next_id=nextId,
@@ -94,12 +141,18 @@ def search(keyword: str, category: str, sort: str):
         nextId = data.get("nextId", None)
         if not nextId:
             break
-        print(f"第{count}页")
-        filter_data(data.get("data", []), keyword)
+        common_logger.info(f"第{count}页")
+        search_result = filter_data(data.get("data", []), keyword)
+        if len(search_result) > 0:
+            result_logger.info(search_result)
+        result.extend(search_result)
         count += 1
         if count % 30 == 0:
-            print("避免风控，休息半分钟")
+            common_logger.info("避免风控，休息半分钟")
             time.sleep(30)
+
+    common_logger.info(f"共找到{len(result)}个结果")
+    common_logger.info(result)
 
 
 @click.command()
